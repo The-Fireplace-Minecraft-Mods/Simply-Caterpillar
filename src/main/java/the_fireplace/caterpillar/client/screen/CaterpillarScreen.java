c15772a7831c2ecc081e281a2620bbf4522804d7
Daniel Mendes <contact@daniel-mendes.ch>
2022-09-01 17:01:11 +0200

    refactor: Use ParchmentMC mappings

--- src/main/java/the_fireplace/caterpillar/client/screen/CaterpillarScreen.java
+++ src/main/java/the_fireplace/caterpillar/client/screen/CaterpillarScreen.java
@@ -9,24 +9,36 @@ import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import the_fireplace.caterpillar.client.screen.util.ScreenTabs;
 import the_fireplace.caterpillar.common.block.entity.DrillHeadBlockEntity;
-import the_fireplace.caterpillar.common.container.CaterpillarContainer;
+import the_fireplace.caterpillar.common.menu.CaterpillarMenu;
 
-public class CaterpillarScreen extends AbstractContainerScreen<CaterpillarContainer> {
+@OnlyIn(Dist.CLIENT)
+public class CaterpillarScreen extends AbstractContainerScreen<CaterpillarMenu> {
+
+    private CaterpillarMenu serverContainer;
 
     private float scrollOffs;
 
     private boolean scrolling;
 
-    public CaterpillarScreen(CaterpillarContainer container, Inventory playerInventory, Component title) {
-        super(CaterpillarContainer.getCurrentContainer(), playerInventory, title);
-        super.imageWidth = 176;
-        super.imageHeight = isReinforcementTab() ? 189 : 166;
-        super.leftPos = 0;
-        super.topPos = 0;
+
+    private static final int SCROLLER_WIDTH = 17;
+    private static final int SCROLLER_HEIGHT = 54;
+
+    public CaterpillarScreen(CaterpillarMenu container, Inventory playerInventory, Component title) {
+        super(container, playerInventory, title);
     }
 
+    @Override
+    protected void init() {
+        super.init();
+        this.serverContainer = CaterpillarMenu.getServerContainer();
+        super.imageWidth = 176;
+        super.imageHeight = isReinforcementTab() ? 189 : 166;
+    }
     @Override
     public void render(PoseStack stack, int mouseX, int mouseY, float partialTicks) {
         super.render(stack, mouseX, mouseY, partialTicks);
@@ -36,13 +48,13 @@ public class CaterpillarScreen extends AbstractContainerScreen<CaterpillarContai
 
     @Override
     protected void renderLabels(PoseStack stack, int mouseX, int mouseY) {
-        Component title = this.menu.getSelectedTab().name;
+        Component title = this.serverContainer.getSelectedTab().name;
         super.titleLabelX = (this.imageWidth - this.font.width(title)) / 2;
         super.titleLabelY = isReinforcementTab() || isDrillHeadTab() ? -10 : 6;
         super.inventoryLabelY = super.imageHeight - 94;
         int titleLabelColor = isReinforcementTab() || isDrillHeadTab() ? 0xFFFFFF : 0x404040;
 
-        switch (this.menu.getSelectedTab()) {
+        switch (this.serverContainer.getSelectedTab()) {
             case DECORATION:
             case INCINERATOR:
             case REINFORCEMENT:
@@ -62,8 +74,8 @@ public class CaterpillarScreen extends AbstractContainerScreen<CaterpillarContai
     }
 
     @Override
-    protected void renderBg(PoseStack stack, float mouseX, int mouseY, int partialTicks) {
-        renderBackground(stack);
+    protected void renderBg(PoseStack stack, float partialTicks, int mouseX, int mouseY) {
+        super.renderBackground(stack);
         bindTexture();
 
         super.imageHeight = isReinforcementTab() ? 189 : 166;
@@ -73,12 +85,11 @@ public class CaterpillarScreen extends AbstractContainerScreen<CaterpillarContai
             renderScrollBar(stack);
         }
     }
-
     private void renderScrollBar(PoseStack stack) {
         int i = this.leftPos + 156;
-        int j = this.topPos + 17;
-        int k = j + 54;
-        blit(stack, i, j + (int)((float)(k - j - 17) * this.scrollOffs), 176, 0, 12, 15);
+        int j = this.topPos + SCROLLER_WIDTH;
+        int k = j + SCROLLER_HEIGHT;
+        blit(stack, i, j + (int)((float)(k - j - SCROLLER_WIDTH) * this.scrollOffs), 176, 0, 12, 15);
 
         int m = 9;
         int n = (int)((double)(scrollOffs * (float)m) + 0.5D);
@@ -101,15 +112,15 @@ public class CaterpillarScreen extends AbstractContainerScreen<CaterpillarContai
                     tabName = tabName.substring(0, 3) + "...";
                 }
 
-                if (this.menu.getSelectedTab().equals(tab)) {
+                if (this.serverContainer.getSelectedTab().equals(tab)) {
                     this.addRenderableWidget(new ImageButton(this.leftPos - 28, this.topPos + 3 + incrementTabPos*20, 31, 20, 176 , 58 + 20, 0, ScreenTabs.DRILL_HEAD.resourceLocation, (onPress) -> {
-                        this.menu.setSelectedTab(tab);
+                        this.serverContainer.setSelectedTab(tab);
                     }));
 
                     this.font.draw(stack, tabName, this.leftPos - 22, this.topPos + incrementTabPos*20 + 9, ChatFormatting.BLACK.getColor());
                 } else {
                     this.addRenderableWidget(new ImageButton(this.leftPos - 31, this.topPos + incrementTabPos*20 + 3 , 31, 20, 176 , 58, 0, ScreenTabs.DRILL_HEAD.resourceLocation, (onPress) -> {
-                        this.menu.setSelectedTab(tab);
+                        this.serverContainer.setSelectedTab(tab);
                     }));
 
                     this.font.draw(stack, tabName, this.leftPos - 25, this.topPos + incrementTabPos*20 + 9, ChatFormatting.GRAY.getColor());
@@ -127,46 +138,46 @@ public class CaterpillarScreen extends AbstractContainerScreen<CaterpillarContai
     private boolean checkShowTab(ScreenTabs tab) {
         switch (tab) {
             case DRILL_HEAD:
-                return this.menu.getDrillHead() != null;
+                return this.serverContainer.getDrillHead() != null;
             case DECORATION:
-                return this.menu.getDecoration() != null;
+                return this.serverContainer.getDecoration() != null;
             case INCINERATOR:
-                return this.menu.getIncinerator() != null;
+                return this.serverContainer.getIncinerator() != null;
             case REINFORCEMENT:
-                return this.menu.getReinforcement() != null;
+                return this.serverContainer.getReinforcement() != null;
             default:
                 return false;
         }
     }
 
     private boolean isReinforcementTab() {
-        return this.menu.getSelectedTab() == ScreenTabs.REINFORCEMENT;
+        return this.serverContainer.getSelectedTab() == ScreenTabs.REINFORCEMENT;
     }
 
     private boolean isDrillHeadTab() {
-        return this.menu.getSelectedTab() == ScreenTabs.DRILL_HEAD;
+        return this.serverContainer.getSelectedTab() == ScreenTabs.DRILL_HEAD;
     }
 
     private boolean isDecorationTab() {
-        return this.menu.getSelectedTab() == ScreenTabs.DECORATION;
+        return this.serverContainer.getSelectedTab() == ScreenTabs.DECORATION;
     }
 
     @Override
-    public boolean mouseReleased(double mouseX, double mouseY, int mouseSide) {
-        if (this.menu.getSelectedTab() == ScreenTabs.DECORATION) {
-            if (mouseSide == 0) {
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        if (this.serverContainer.getSelectedTab() == ScreenTabs.DECORATION) {
+            if (button == 0) {
                 this.scrolling = false;
                 return true;
             }
         }
 
-        return super.mouseReleased(mouseX, mouseY, mouseSide);
+        return super.mouseReleased(mouseX, mouseY, button);
     }
 
     @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int mouseSide) {
-        if (this.menu.getSelectedTab() == ScreenTabs.DECORATION) {
-            if (mouseSide == 0) {
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (this.serverContainer.getSelectedTab() == ScreenTabs.DECORATION) {
+            if (button == 0) {
                 if (this.insideScrollBar(mouseX, mouseY)) {
                     this.scrolling = true;
                     return true;
@@ -174,50 +185,49 @@ public class CaterpillarScreen extends AbstractContainerScreen<CaterpillarContai
             }
         }
 
-        return super.mouseClicked(mouseX, mouseY, mouseSide);
+        return super.mouseClicked(mouseX, mouseY, button);
     }
 
     private boolean insideScrollBar(double mouseX, double mouseY) {
         int i = this.leftPos + 156;
-        int j = this.topPos + 17;
-        int k = j + 54;
+        int j = this.topPos + SCROLLER_WIDTH;
+        int k = j + SCROLLER_HEIGHT;
         return mouseX >= (double)i && mouseY >= (double)j && mouseX < (double)i + 12 && mouseY < (double)k;
     }
 
     @Override
-    public boolean mouseDragged(double mouseX, double mouseY, int mouseSide, double p_97755_, double p_97756_) {
-        if (this.menu.getSelectedTab() == ScreenTabs.DECORATION) {
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double dragX, double dragY) {
+        if (this.serverContainer.getSelectedTab() == ScreenTabs.DECORATION) {
             if (this.scrolling) {
-                int i = this.topPos + 17;
-                int j = i + 54;
+                int i = this.topPos + SCROLLER_WIDTH;
+                int j = i + SCROLLER_HEIGHT;
                 this.scrollOffs = ((float)mouseY - (float)i - 7.5F) / ((float)(j - i) - 15.0F);
                 this.scrollOffs = Mth.clamp(this.scrollOffs, 0.0F, 1.0F);
-                // this.menu.scrollTo(this.scrollOffs);
+                // this.serverContainer.scrollTo(this.scrollOffs);
                 return true;
             }
 
-            return super.mouseDragged(mouseX, mouseY, mouseSide, p_97755_, p_97756_);
+            return super.mouseDragged(mouseX, mouseY, button, dragX, dragY);
         }
 
         return false;
     }
 
     @Override
-    public boolean mouseScrolled(double mouseX, double mouseY, double mouseWheelDirection) {
-        if (this.menu.getSelectedTab() == ScreenTabs.DECORATION) {
+    public boolean mouseScrolled(double mouseX, double mouseY, double delta) {
+        if (this.serverContainer.getSelectedTab() == ScreenTabs.DECORATION) {
             int i = 9;
-            float f = (float)(mouseWheelDirection / (double)i);
+            float f = (float)(delta / (double)i);
             this.scrollOffs = Mth.clamp(this.scrollOffs - f, 0.0F, 1.0F);
-            // this.menu.decoration.scrollTo(this.scrollOffs);
+            // this.serverContainer.decoration.scrollTo(this.scrollOffs);
             return true;
         }
 
         return false;
     }
-
-    private void bindTexture() {
+     private void bindTexture() {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, this.menu.getSelectedTab().resourceLocation);
+        RenderSystem.setShaderTexture(0, this.serverContainer.getSelectedTab().resourceLocation);
     }
 }
