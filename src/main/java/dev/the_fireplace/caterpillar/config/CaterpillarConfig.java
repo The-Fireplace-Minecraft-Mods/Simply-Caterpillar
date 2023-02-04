3ca69d22c3a116e88ccdb375a9ab7a128f00d0d3
Daniel Mendes <contact@daniel-mendes.ch>
2023-02-04 18:16:40 +0100

    Added tooltip to config breakUnbreakableBlocks

--- src/main/java/dev/the_fireplace/caterpillar/config/CaterpillarConfig.java
+++ src/main/java/dev/the_fireplace/caterpillar/config/CaterpillarConfig.java
@@ -3,12 +3,17 @@ package dev.the_fireplace.caterpillar.config;
 import dev.the_fireplace.caterpillar.Caterpillar;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = Caterpillar.MOD_ID)
 public class CaterpillarConfig implements ConfigData {
 
     public static boolean firstUse = true;
+
     public static boolean useParticles = true;
+
+    @ConfigEntry.Gui.Tooltip
     public static boolean breakUnbreakableBlocks = false;
+
     public static boolean enableSounds = true;
 }
